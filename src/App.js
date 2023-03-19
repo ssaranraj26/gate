e85d3918c0ac76aba3074e3e83b1b218da1875e1
Saranraj <ssaranraj2698@gmail.com>
2023-03-19 16:28:12 +0000

    added home

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Route, Switch} from 'react-router-dom'
+import Login from './components/Login'
+import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+  </Switch>
+)
+
+export default App
